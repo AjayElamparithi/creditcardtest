a9b8ffd23c9caa01b1bef267bb3c76271df55659
Ajaycloobot <ajay@cloobot.com>
2021-07-26 11:56:57 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
+import './components/CreditCardForm';
 import './App.css';
-
+import CreditCardForm from "./components/CreditCardForm";
+// import './index.test';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CreditCardForm />
     </div>
   );
 }
